43dc9bfb8bbd1eb2e1379c9ea631cd145f0acf00
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-03-28 23:25:34 +0100

    build and test against multiple version of Clojure

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.brunobonacci/clj-sophia "0.3.0"
-  :description "a Clojure driver for sophia DB."
+  :description "a Clojure driver for Sophia DB."
+
   :url "https://github.com/BrunoBonacci/clj-sophia"
 
   :license {:name "Apache License 2.0"
@@ -7,15 +8,24 @@
 
   :scm {:name "git" :url "https://github.com/BrunoBonacci/clj-sophia"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.java.dev.jna/jna "4.5.1"]
+  :dependencies [[net.java.dev.jna/jna "4.5.1"]
                  [com.taoensso/nippy "2.14.0"]
                  [prismatic/schema "1.1.7"]]
 
-  :profiles {:dev {:dependencies [[midje "1.9.1"]
-                                  [org.clojure/test.check "0.9.0"]
-                                  [criterium "0.4.4"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
+  :profiles
+  {:1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+
+   :dev [:1.9
+         {:dependencies [[midje "1.9.1"]
+                         [org.clojure/test.check "0.9.0"]
+                         [criterium "0.4.4"]]
+          :plugins [[lein-midje "3.2.1"]]}]}
+
+  :aliases
+  {"test-all"   ["do" "clean,"
+                 "with-profile" "+1.9:+1.8" "midje"]}
 
   :jvm-opts ["-server"
              #_"-Djna.library.path=./lib/sophia"
