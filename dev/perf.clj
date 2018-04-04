1c080fa806af4cc624f1191857e49d5561747279
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-04-04 20:09:41 +0100

    update perf test with cursor reflection fix

--- dev/perf.clj
+++ dev/perf.clj
@@ -1,6 +1,7 @@
 (ns perf
   (:require [com.brunobonacci.sophia :as sph]
-            [criterium.core :refer [bench quick-bench]]))
+            [criterium.core :refer [bench quick-bench]]
+            [clojure.string :as str]))
 
 
 
@@ -37,7 +38,7 @@
   ;; Overhead used : 2.086444 ns
   ;;
   ;; Found 3 outliers in 60 samples (5.0000 %)
-  ;; low-severe	 3 (5.0000 %)
+  ;; low-severe  3 (5.0000 %)
   ;; Variance from outliers : 43.4703 % Variance is moderately inflated by outliers
 
 
@@ -49,6 +50,14 @@
 ;;                                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+  ;; round up keys up to 5M
+  (let [nkeys (with-open [^java.io.Closeable cur (sph/cursor env)]
+                (count (sph/range-query cur "perf")))]
+    (when (> nkeys 5000000)
+      (throw (ex-info "Too many keys in db." {:nkeys nkeys})))
+    ;; add more keys up to 5M
+    (dotimes [_ (- 5000000 nkeys)]
+      (sph/set-value!  env "perf" (uuid) (uuid))))
 
   ;; get 100 random keys
   (def ids
@@ -56,7 +65,7 @@
      (with-open [cur (sph/cursor env)]
        (doall
         (take 100
-              (partition 15000
+              (partition 50000
                          (sph/range-query cur "perf")))))
      (mapv ffirst)))
 
@@ -68,16 +77,16 @@
    (sph/get-value env "perf" (rand-nth ids)))
 
 
-  ;; Evaluation count : 3508320 in 60 samples of 58472 calls.
-  ;; Execution time mean : 17.593094 µs
-  ;; Execution time std-deviation : 294.678938 ns
-  ;; Execution time lower quantile : 17.190567 µs ( 2.5%)
-  ;; Execution time upper quantile : 18.315122 µs (97.5%)
-  ;; Overhead used : 2.086444 ns
+  ;; Evaluation count : 3338580 in 60 samples of 55643 calls.
+  ;; Execution time mean : 18.583898 µs
+  ;; Execution time std-deviation : 633.802589 ns
+  ;; Execution time lower quantile : 17.670723 µs ( 2.5%)
+  ;; Execution time upper quantile : 19.701369 µs (97.5%)
+  ;; Overhead used : 1.811818 ns
   ;;
-  ;; Found 3 outliers in 60 samples (5.0000 %)
-  ;; low-severe	 3 (5.0000 %)
-  ;; Variance from outliers : 6.2541 % Variance is slightly inflated by outliers
+  ;; Found 1 outliers in 60 samples (1.6667 %)
+  ;; low-severe  1 (1.6667 %)
+  ;; Variance from outliers : 20.6156 % Variance is moderately inflated by outliers
 
 
 
@@ -89,35 +98,38 @@
 
 
   (defn rand-prefix []
-    (->> (repeatedly #(rand-nth '("0" "1" "2" "3" "4" "5" "6" "7" "8" "a" "b" "c" "d" "e" "f")))
-         (take 8)
-         (apply str)))
+    (-> (rand-nth ids)
+        (subs 0 8)))
 
-  (bench (rand-prefix)) ;; 3.415691 µs
+  (bench (rand-prefix)) ;; 152.616500 ns
 
   ;; seek time
   (bench
    (with-open [^java.io.Closeable cur (sph/cursor env)]
      (first (sph/range-query cur "perf" :key (rand-prefix)))))
 
-  ;; Evaluation count : 1240320 in 60 samples of 20672 calls.
-  ;; Execution time mean : 51.623567 µs (- 3.415691 µs) => ~*48.2 µs*
-  ;; Execution time std-deviation : 3.866012 µs
-  ;; Execution time lower quantile : 45.745753 µs ( 2.5%)
-  ;; Execution time upper quantile : 57.400526 µs (97.5%)
-  ;; Overhead used : 2.086444 ns
+  ;; Evaluation count : 1398900 in 60 samples of 23315 calls.
+  ;; Execution time mean : 45.553131 µs
+  ;; Execution time std-deviation : 3.049912 µs
+  ;; Execution time lower quantile : 40.749923 µs ( 2.5%)
+  ;; Execution time upper quantile : 49.009489 µs (97.5%)
+  ;; Overhead used : 1.811818 ns
+  ;;
+  ;; Found 1 outliers in 60 samples (1.6667 %)
+  ;; low-severe  1 (1.6667 %)
+  ;; Variance from outliers : 50.1159 % Variance is severely inflated by outliers
 
 
   ;; scan time
   (println
    "count:"
    (time
-    (with-open [^java.io.Closeablecur (sph/cursor env)]
+    (with-open [^java.io.Closeable cur (sph/cursor env)]
       (count (sph/range-query cur "perf")))))
 
-  ;; "Elapsed time: 41259.694217 msecs"
-  ;; count: 2413142
-  ;; (/ (* 1000 41259.694217) 2413142 ) => 17.097913930054677 µs
+  ;; "Elapsed time: 40457.261344 msecs"
+  ;; count: 5000000
+  ;; (/ (* 1000 86066.752595) 5000000 ) => 17.213350519 µs
 
 
   )
